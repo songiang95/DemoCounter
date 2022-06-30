e562d4f19596a01759e9f7a0d45b40f131e3e964
songiang95 <songiang1995@gmail.com>
2022-06-30 17:17:02 +0700

    Initial commit

---
+++ demo_flutter/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:demo_flutter/provider/counter_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(//              <-- ChangeNotifierProvider
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Consumer<CounterModel>( //                 <-- Consumer
+                  builder: (ctx, counterModel, _) {
+                    return Text(
+                      '${counterModel.counter}',
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => context.read<CounterModel>().increase(), //       <-- Provide.of
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
+      create: (ctx) => CounterModel(),
+    );
+  }
+}
